9f7842df76199a95061ee9324ddf7b5c994da29e
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-10-14 22:26:59 +0500

    Task4

--- Tyuiu.skirnevskyBR.Sprint2.Task4.V27.Lib/DataService.cs
+++ Tyuiu.skirnevskyBR.Sprint2.Task4.V27.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.skirnevskyBR.Sprint2.Task4.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = (x - 10) < (y + 2) ? (x * Math.Pow((y + 2) / (x - 1), x)) : (Math.Pow(y, 2) - (2 / x));
+            double z = (x - 10) > (y + 2) ? (x * Math.Pow(((y + 2) / (x - 1)), x)) : (Math.Pow(y, 2) - (2 / x));
             return Math.Round(z, 3);
         }
     }
